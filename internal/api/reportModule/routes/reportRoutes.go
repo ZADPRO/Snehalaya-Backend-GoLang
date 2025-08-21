5412b63a9f2649188d7c569b0bb1345f37a2db5e
ionhax-dev <thirukumara.d@zadroit.com>
2025-08-21 15:18:49 +0530

    report products - api - product with pagination

--- internal/api/reportModule/routes/reportRoutes.go
+++ internal/api/reportModule/routes/reportRoutes.go
@@ -1,6 +1,7 @@
 package reportRoutes
 
 import (
+	reportController "github.com/ZADPRO/Snehalaya-Backend-GoLang/internal/api/reportModule/controller"
 	accesstoken "github.com/ZADPRO/Snehalaya-Backend-GoLang/internal/helper/AccessToken"
 	"github.com/gin-gonic/gin"
 )
@@ -8,5 +9,5 @@ import (
 func ReportRoutes(router *gin.Engine) {
 	route := router.Group("/api/v1/admin/reports")
 
-	route.POST("/productReports", accesstoken.JWTMiddleware())
+	route.POST("/productReports", accesstoken.JWTMiddleware(), reportController.GetAllProductsReportController())
 }
